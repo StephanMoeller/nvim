3268dd30ccbc4ca1bab1030d2185a62fbc72f48f
Stephan <stephanryer@hotmail.com>
2023-12-22 12:36:31 +0100

    hey

--- lua/rollercole/remap.lua
+++ lua/rollercole/remap.lua
@@ -3,8 +3,8 @@ vim.wo.number = true
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ft", vim.cmd.Ex);
-vim.keymap.set("n", "<leader>oa", ":q<Enter>");
-vim.keymap.set("n", "<leader>rs", ":wq<Enter>");
+vim.keymap.set("n", "<leader>s<leader>", ":q<Enter>");
+vim.keymap.set("n", "<leader>a<leader>", ":wq<Enter>");
 vim.keymap.set("n", "U", "<c-r>"); -- Redo
 vim.keymap.set("n", "<enter>", "o<enter>");
 vim.keymap.set("n", "<enter>", "o<enter>");
